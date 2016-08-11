686273eb1ef93eed174e27e1a3106fe5ce3cf27b
vdoraira <venkatram.dorairaj@gmail.com>
2016-08-11 11:03:57 +0530

    first commit

---
+++ Client/src/js/Components/NavChildComponents.js
@@ -0,0 +1,58 @@
+var React= require("react");
+var NavChildComponents=React.createClass({
+ render:function(){
+   return(
+     <div>
+  <nav className="navbar navbar-default navbar-inverse navbar-collapse">
+<div className="container-fluid">
+
+<div className="navbar-header">
+<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+<span className="sr-only">Toggle navigation</span>
+<span className="icon-bar"></span>
+<span className="icon-bar"></span>
+<span className="icon-bar"></span>
+</button>
+</div>
+
+
+<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<form className="navbar-form navbar-left">
+<div className="form-group">
+  <input type="text" className="form-control" placeholder="Search"/>
+  <button type="button" className="btn btn-default ">
+    <span className="glyphicon glyphicon-search"></span>
+  </button>
+</div>
+</form>
+
+
+<ul className="nav navbar-nav navbar-collapse">
+<li className="dropdown">
+  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="../images/jailani.jpg" alt="Smiley face" width="30" height="30" /> Jailani S M A <span className="caret"></span></a>
+  <ul className="dropdown-menu">
+    <li><a href="#">MyAccount</a></li>
+    <li role="separator" className="divider"></li>
+    <li><a href="#">Signout</a></li>
+  </ul>
+</li>
+</ul>
+
+<ul>
+<li className="collapse navbar-collapse navbar-right " id="logo">
+<img id="gmaillogo" src="../images/gmail.jpg" alt="Smiley face"/>
+</li>
+</ul>
+
+</div>
+
+
+
+</div>
+</nav>
+  </div>
+
+         )
+   }
+})
+module.exports=NavChildComponents;
